6085f6157d3b0a81794755bc5d75e31a168c8d14
pattyouwehand <pattyouwehand@gmail.com>
2019-08-22 14:18:41 +0200

    setup react

---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+const initialState = [
+  {
+    id: 1,
+    name: 'Pizza Margarita',
+    description: 'Minimalism is key!',
+    ingredients: [
+      'tomato',
+      'mozarella',
+      'basil'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Pizza Napoletana',
+    description: 'Like Margarita, but without the basil.',
+    ingredients: [
+      'tomato',
+      'mozarella'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Pizza Bianca',
+    description: 'Did somebody say oil?',
+    ingredients: [
+      'olive oil',
+      'salt',
+      'rosemary'
+    ]
+  }
+]
+
+const reducer = (state = initialState, action = {}) => {
+  switch(action.type){
+    case 'SET_PIZZA_ID':
+      return [
+        ...state,
+        {...action.payload}
+      ]
+    default:
+      return state
+  }
+}
+
+export default reducer
